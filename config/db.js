32e60c6ea7ba98a1d8b79f3ab2cf8cc1ccb24288
reinaldozhu6-web <reinaldozhu6@gmail.com>
2025-10-04 19:24:42 -0400

    Final

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 module.exports = function () {
+
+    const uri =
+        process.env.MONGODB_URI ||
+        "mongodb+srv://admin_db_user:keK4QJs8yXcuYnXm@cluster003.biow1it.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster003";
+
     mongoose
-        .connect(
-            "mongodb+srv://admin_db_user:keK4QJs8yXcuYnXm@cluster003.biow1it.mongodb.net/Portfolio"
-        )
+        .connect(uri)
         .then(() => console.log("✅ Connected to MongoDB"))
         .catch((err) => console.error("❌ MongoDB connection error:", err));
 };
